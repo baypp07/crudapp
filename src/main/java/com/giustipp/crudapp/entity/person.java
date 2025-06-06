ed9e08fe5c00c3b9febce4d1ef436011ecd0f3e8
baypp07 <pphouone@gmail.com>
2025-06-06 11:57:49 +0200

    create db hibernate jarkatar
    table person to mysql

---
+++ src/main/java/com/giustipp/crudapp/entity/person.java
@@ -0,0 +1,58 @@
+package com.giustipp.crudapp.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="person")
+public class person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name ="firstname")
+    private String fname;
+
+    @Column(name="lastname")
+    private String lname;
+
+    public person(String fname, String lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    @Override
+    public String toString() {
+        return "person{" +
+                "id=" + id +
+                ", fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                '}';
+    }
+
+
+
+}
